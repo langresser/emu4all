fbb82b211ed044c26320b9a2937e7dba0bdf4a21
wangjia <wangjiatc@gmail.com>
2013-03-14 20:17:11 +0800

    11

--- GBA.emu/metadata/conf.mk
+++
@@ -1,8 +0,0 @@
-include ../EmuFramework/metadata/conf.mk
-metadata_name = GBA.emu
-metadata_exec = gbaemu
-metadata_pkgName = GbaEmu
-webOS_metadata_id = com.explusalpha.gbaemu
-metadata_id = com.explusalpha.GbaEmu
-metadata_vendor = Robert Broglia
-webOS_metadata_requiredMemory = 64
